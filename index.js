dad561e38fb53d8d52d970fc1e95ee045235c995
Shifat507 <ferdoushasanshifat@gmail.com>
2024-12-26 22:53:49 +0600

    all done

--- index.js
+++ index.js
@@ -5,7 +5,14 @@ const { MongoClient, ServerApiVersion, ObjectId, Timestamp } = require('mongodb'
 const port = process.env.PORT || 5000
 const app = express()
 
-app.use(cors())
+app.use(cors({
+    origin: [
+        'http://localhost:5173',
+        'https://trust-lens.web.app',
+        'https://trust-lens.firebaseapp.com'
+    ],
+    credentials: true
+}))
 app.use(express.json())
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vyhfd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
@@ -141,10 +148,10 @@ async function run() {
 
 
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+        // await client.connect();
         // Send a ping to confirm a successful connection
-        await client.db("admin").command({ ping: 1 });
-        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+        // await client.db("admin").command({ ping: 1 });
+        // console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
         // await client.close();
